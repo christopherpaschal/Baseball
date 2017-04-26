8e69b47b0042d0a7905a71d3e48c36bd9b403366
christopherpaschal <christopher.a.paschal@gmail.com>
2017-04-26 12:19:30 -0400

    added WS dots and tooltips

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // set the dimensions and margins of the graph
 var margin = {top: 25, right: 25, bottom: 25, left: 25},
-    width = 800 - margin.left - margin.right,
+    width = 1200 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
 
@@ -29,19 +29,19 @@ d3.csv("data/Teams.csv", function(error, data) {
       d.HomeWins = +d.Ghome;
       d.Wins = +d.W;
       d.Year = +d.yearID;
-      d.Team = d.teamID;
+      d.Team = d.franchID;
 
   });
 
   var dataBOS = data.filter(function(d) {
-    return d.Team == "ATL" && d.Year >= 2000;
+    return d.Team == "BOS" && d.Year >= 1900;
   }).sort(function(a,b) {return b.Year - a.Year;});
 
   var dataATL = data.filter(function(d) {
-    return d.Team == "CHN" && d.Year >= 2000;
+    return d.Team == "ATL" && d.Year >= 1900;
   }).sort(function(a,b) {return b.Year - a.Year;});
 
-  xScale.domain([2000, 2016]);
+  xScale.domain([1900, 2016]);
   yScale.domain([0, 162]);
 
   // define the line
@@ -97,18 +97,62 @@ d3.csv("data/Teams.csv", function(error, data) {
       .attr("r", 4)
       .attr("cx", function(d) { return xScale(d.Year); })
       .attr("cy", function(d) { return yScale(d.Wins); })
-      .attr("fill", "#ffffff")
-      .attr("opacity", 0);
+      .attr("fill", "#ff0000")
+      .attr("opacity", function(d) {
+        if (d.WSWin == "Y") {
+          return 1;
+        } else {
+          return 0;
+        }
+      }).on("mouseover", function(d) {
+        d3.select(this).attr("opacity", 1)
+        tooltip.style("opacity", 1);
+        tooltip.html("Year: " + d.Year + "<br/>" + "Wins: " + d.Wins)
+          .style("left", d3.event.pageX + 5 + "px")
+          .style("top", d3.event.pageY + 5 + "px")
+      })
+      .on("mouseout", function(d) {
+          d3.select(this).attr("opacity",  function(d) {
+            if (d.WSWin == "Y") {
+              return 1;
+            } else {
+              return 0;
+            }
+          });
+          tooltip.style("opacity", 0);
+      });
 
-  //add scatterplot to overlay on line graph
-  svg.selectAll("dot")
-      .data(dataATL)
-  .enter().append("circle")
-      .attr("class", "dot")
-      .attr("r", 4)
-      .attr("cx", function(d) { return xScale(d.Year); })
-      .attr("cy", function(d) { return yScale(d.Wins); })
-      .attr("fill", "#ffffff")
-      .attr("opacity", 0);
+    //add scatterplot to overlay on line graph
+    svg.selectAll("dot")
+        .data(dataATL)
+    .enter().append("circle")
+        .attr("class", "dot")
+        .attr("r", 4)
+        .attr("cx", function(d) { return xScale(d.Year); })
+        .attr("cy", function(d) { return yScale(d.Wins); })
+        .attr("fill", "#0000ff")
+        .attr("opacity", function(d) {
+          if (d.WSWin == "Y") {
+            return 1;
+          } else {
+            return 0;
+          }
+        }).on("mouseover", function(d) {
+          d3.select(this).attr("opacity", 1)
+          tooltip.style("opacity", 1);
+          tooltip.html("Year: " + d.Year + "<br/>" + "Wins: " + d.Wins)
+            .style("left", d3.event.pageX + 5 + "px")
+            .style("top", d3.event.pageY + 5 + "px");
+        })
+        .on("mouseout", function(d) {
+            d3.select(this).attr("opacity",  function(d) {
+              if (d.WSWin == "Y") {
+                return 1;
+              } else {
+                return 0;
+              }
+            });
+            tooltip.style("opacity", 0);
+        });
 
 })
